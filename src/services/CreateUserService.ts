a9646811348f2371a9d9679fc9cf00527793840e
Risoaldo Nóbrega <noobregarisoaldo@gmail.com>
2021-01-12 23:58:52 -0300

    ajustes user

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+
+interface Request {
+    name: string;
+    email: string;
+    password: string;
+}
+
+class CreateUserService {
+
+    public async execute({ name, email, password }: Request): Promise<User> {
+        const userRepository = getRepository(User);
+
+        const userExists = await userRepository.findOne({
+            where: { email },
+        });
+
+        if (userExists) {
+            throw new Error("User Exists");
+        }
+
+        const hashPassword = await hash(password, 8);
+        
+        const user = userRepository.create({
+            name,
+            email,
+            password: hashPassword,
+        });
+
+        await userRepository.save(user);
+
+        return user;
+    }
+
+}
+
+export default CreateUserService;
